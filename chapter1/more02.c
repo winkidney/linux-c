94a614018f8f043817b92956d49f67609a8a8d7d
root <root@kidney-PAN4.(none)>
2014-03-01 14:04:55 +0800

    ADD: add moe02.c

---
+++ chapter1/more02.c
@@ -0,0 +1,62 @@
+/*this is a program that does what the "more" command can do .
+ *more02.c
+ */
+#include<stdio.h>
+#define PAGELEN 24
+#define LINELEN 512
+//int exit();     //如果没有这个声明，那么gcc编译将会产生警告，因为第一次遇到使用exit函数的时候却未发现声明？
+/*如果有上面这行，gcc将会产生和内建函数冲突的警告，所以索性还是不要理会吧 = =以后再来看个究竟。*/
+void do_more(FILE *);
+int see_more();
+
+int main(int argc, char *argv[]){
+    FILE *fp;
+    if (argc == 1)
+        do_more(stdin);
+    else
+        while(--argc)
+            if ( (fp = fopen(*++argv,"r")) != NULL ){
+                    do_more( fp );
+                    fclose( fp );
+            }
+            else
+                exit(1);
+    return 0;
+}
+
+void do_more( FILE * fp){
+    char line[LINELEN];
+    int num_of_lines = 0;
+    int see_more(FILE *),reply;
+    FILE * fp_tty;
+    fp_tty = fopen( "/dev/tty", "r" );  /*NEW:cmd stream*/
+    if (fp_tty == NULL)
+        exit(1);
+    while (fgets( line, LINELEN, fp)){  /*more input*/
+        if (num_of_lines == PAGELEN){   /*full screen?*/
+            reply = see_more(fp_tty);   /*NEW:pass a FILE *  */
+            if (reply == 0)
+                break;
+            num_of_lines -= reply;  /*reset count*/
+        }
+        if (fputs(line, stdout) == EOF)
+            exit(1);
+        num_of_lines++;
+    }
+}
+
+
+int see_more(FILE * cmd){   /*NEW: accepts arg */
+    int c;
+    printf("\033[7m more?\033[m");
+    while( (c=getc(cmd)) != EOF ){      /*NEW:reads from tty*/
+        if ( c == 'q' )
+            return 0;
+        if ( c == ' ' )
+            return PAGELEN;
+        if ( c == '\n' )
+            return 1;
+    }
+    return 0;
+}
+
